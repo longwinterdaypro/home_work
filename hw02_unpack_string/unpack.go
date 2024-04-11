335985d69d862af5124ccfa1c5fcaf31069d5d84
Aleksei Grigorev <info@longwinterday-pro.ru>
2024-04-11 20:04:59 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	sb := &strings.Builder{}
+	var sub string
+	var screen bool
+
+	for _, char := range s {
+		switch {
+		case (48 <= char && char <= 57 && !screen) || char == 10:
+			if sub == "" || char == 10 {
+				return "", ErrInvalidString
+			}
+			num, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", err
+			}
+			sb.WriteString(strings.Repeat(sub, num))
+			sub = ""
+		case char == 92 && !screen:
+			screen = true
+		default:
+			if sub != "" {
+				sb.WriteString(sub)
+				screen = false
+			}
+			sub = string(char)
+		}
+	}
+	sb.WriteString(sub)
+	return sb.String(), nil
 }
